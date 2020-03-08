51bef7833c96a18dd5cd204e524f4456400d7c15
Nuriza <nuriza.samidinovna@gmail.com>
2020-03-09 00:25:54 +0600

    lesson 61. About Generics

---
+++ src/main/java/com/company/Main.java
@@ -0,0 +1,21 @@
+package com.company;
+
+public class Main {
+    //  public volatile static int place=1;
+    public static void main(String[] args) {
+//Item item=new Item();
+//item.start();
+//Money money=new Money();
+//money.start();
+//        ThreadQ t1 = new ThreadQ("A");
+//        ThreadQ t2 = new ThreadQ("B");
+//        ThreadQ t3 = new ThreadQ("C");
+//        ThreadQ t4 = new ThreadQ("D");
+//        ThreadQ t5 = new ThreadQ("E");
+//        t1.start();
+//        t2.start();
+//        t3.start();
+//        t4.start();
+//        t5.start();
+    }
+}
